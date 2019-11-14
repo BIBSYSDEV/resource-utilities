2749d089fb16ba3bff21c8079a99c0939f74c77e
brinxmat <rurik.greenall@gmail.com>
2019-11-14 13:42:53 +0100

    Update code to pass standard pylint checks

--- common/validator.py
+++ common/validator.py
@@ -1,30 +1,40 @@
+"""Validation methods for AWS Dynamo DB resources in NVA"""
+
+
 def validate_resource_insert(resource):
+    """Validates resources for insertion into NVA"""
     if resource.resource_identifier is not None:
         raise ValueError('Resource has identifier')
-    elif resource.metadata is None:
+    if resource.metadata is None:
         raise ValueError('Resource has no metadata')
-    elif resource.files is None:
+    if resource.files is None:
         raise ValueError('Resource has no files')
-    elif resource.owner is None:
+    if resource.owner is None:
         raise ValueError('Resource has no owner')
-    elif not isinstance(resource.metadata, dict):
+    if not isinstance(resource.metadata, dict):
         raise ValueError('Resource has invalid attribute type for metadata')
-    elif not isinstance(resource.files, dict):
+    if not isinstance(resource.files, dict):
         raise ValueError('Resource has invalid attribute type for files')
 
 
 def validate_resource_modify(resource):
+    """Validates resources for updating in NVA"""
     if resource.resource_identifier is None:
         raise ValueError('Resource has no identifier')
-    elif resource.metadata is None:
-        raise ValueError('Resource with identifier ' + resource.resource_identifier + ' has no metadata')
-    elif resource.files is None:
-        raise ValueError('Resource with identifier ' + resource.resource_identifier + ' has no files')
-    elif resource.owner is None:
-        raise ValueError('Resource with identifier ' + resource.resource_identifier + ' has no owner')
-    elif not isinstance(resource.metadata, dict):
+    if resource.metadata is None:
+        raise ValueError('Resource with identifier '
+                         + resource.resource_identifier + ' has no metadata')
+    if resource.files is None:
+        raise ValueError('Resource with identifier '
+                         + resource.resource_identifier + ' has no files')
+    if resource.owner is None:
+        raise ValueError('Resource with identifier '
+                         + resource.resource_identifier + ' has no owner')
+    if not isinstance(resource.metadata, dict):
         raise ValueError(
-            'Resource with identifier ' + resource.resource_identifier + ' has invalid attribute type for metadata')
-    elif not isinstance(resource.files, dict):
+            'Resource with identifier '
+            + resource.resource_identifier + ' has invalid attribute type for metadata')
+    if not isinstance(resource.files, dict):
         raise ValueError(
-            'Resource with identifier ' + resource.resource_identifier + ' has invalid attribute type for files')
+            'Resource with identifier '
+            + resource.resource_identifier + ' has invalid attribute type for files')
