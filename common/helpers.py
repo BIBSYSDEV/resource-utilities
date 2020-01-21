fa4e052e71cad5340048c5a143c3cc9ddd081bbb
Jan Bjørndalen <jan.bjorndalen@unit.com>
2020-01-21 08:49:24 +0100

    Split line too long

--- common/helpers.py
+++ common/helpers.py
@@ -4,6 +4,7 @@ from os import environ
 from .constants import Constants
 from .http_constants import HttpConstants
 
+
 def remove_none_values(temp_value):
     """Remove None from dicts to ensure that nulls are ignored"""
     return_value = dict()
@@ -17,7 +18,8 @@ def response(status_code, body):
     """Formulates a response with status code and body"""
     headers = dict()
     if environ.get(Constants.env_var_allowed_origin()) is not None:
-        headers[HttpConstants.http_header_access_control_allow_origin()] = environ.get(Constants.env_var_allowed_origin())
+        headers[HttpConstants.http_header_access_control_allow_origin()] = environ.get(
+            Constants.env_var_allowed_origin())
 
     return {
         Constants.response_status_code(): status_code,
